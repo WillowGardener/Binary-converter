1f6262b6d2365911b720680c7b3bcb98c81c35ba
Griffin Wimminger & Austin Steward <student@epicodus.com>
2014-07-23 15:36:36 -0700

    through an enormously complicated mathematical process, converts base ten numbers to binary :santa::gun::squirrel:

---
+++ lib/binary-converter.rb
@@ -0,0 +1,3 @@
+def converter(number)
+  binary = number.to_i.to_s(2).to_i
+end
---
+++ spec/binary_converter_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('binary-converter')
+
+describe('converter') do
+  it('Returns the binary form of a Base-10 number') do
+    expect(converter("100")).to(eq(1100100))
+  end
+end
